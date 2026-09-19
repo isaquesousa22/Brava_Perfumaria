751dcfba296b4e01a74509b8f3207ee44c43d5f2
isaquesousa22 <isaqueluzsousa83@gmail.com>
2026-09-18 22:11:28 -0300

    login feito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProdutoComponent } from './produto/produto.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     component: HomeComponent
   },
 
@@ -17,5 +18,10 @@ export const routes: Routes = [
   {
     path: 'cadastro',
     component: CadastroComponent
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
